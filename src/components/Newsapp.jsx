63e937a1f6350556fb35ebe118daf9b8c263d7b8
Raj-Hawlader <rajhawlader385.rh@gmail.com>
2024-10-02 23:51:33 +0600

    second commit

--- src/components/Newsapp.jsx
+++ src/components/Newsapp.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { IoSearchOutline } from "react-icons/io5";
 
 const Newsapp = () => {
   return (
-    <div>Newsapp</div>
+    <div>
+        <nav>
+            <div className='flex justify-between mx-11 my-3'>
+                <h2>date:
+                    <br />
+                    Time:
+                </h2>
+                <h1 className='font-mono'>Daily Observer</h1>
+                <IoSearchOutline/>
+            </div>
+        </nav>
+    </div>
   )
 }
 
